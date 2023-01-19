7df6c91b1349f2c8c0399e9ba2e839a0e258dbd0
Matheus Lucas <matheus.lucas1105@gmail.com>
2023-01-19 17:46:52 -0300

    correções

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -18,24 +18,24 @@ class Cart extends React.Component {
     const { cartProducts } = this.state;
     return (
       <div>
-        {cartProducts !== null ? (
-          <div>
-            {cartProducts.map(({ name, quantity, image }, index) => (
-              <div key={ index }>
-                <img src={ image } alt={ name } />
-                <p data-testid="shopping-cart-product-name">{name}</p>
-                <p
-                  data-testid="shopping-cart-product-quantity"
-                >
-                  {`Quantidade: ${quantity}`}
-                </p>
-              </div>
-            ))}
-          </div>)
+        {cartProducts.length === 0 ? (
+          <h3 data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </h3>)
           : (
-            <h3 data-testid="shopping-cart-empty-message">
-              Seu carrinho está vazio
-            </h3>)}
+            <div>
+              {cartProducts.map(({ name, quantity, image }, index) => (
+                <div key={ index }>
+                  <img src={ image } alt={ name } />
+                  <p data-testid="shopping-cart-product-name">{name}</p>
+                  <p
+                    data-testid="shopping-cart-product-quantity"
+                  >
+                    {`Quantidade: ${quantity}`}
+                  </p>
+                </div>
+              ))}
+            </div>)}
 
       </div>
     );
